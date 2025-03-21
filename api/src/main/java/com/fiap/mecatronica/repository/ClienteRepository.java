7b3194f1b9ed1b736fbaf949989e4a1fcfa7d318
BorealAce <gabrielborgesgs20@gmail.com>
2025-03-21 18:14:30 -0300

    Delete do conteúdo antigo

--- api/src/main/java/com/fiap/mecatronica/repository/ClienteRepository.java
+++
@@ -1,4 +0,0 @@
-package com.fiap.mecatronica.repository;
-import com.fiap.mecatronica.api.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
